6f11168213213a828b4132e3069d5798a9760977
Forhad Parvez <forhadparvez@outlook.com>
2019-08-31 22:03:56 +0600

    Add project files.

---
+++ CustomException/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CustomException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = new Student()
+            {
+                Id = 1,
+                Name = "Mr A",
+                Age = 9
+            };
+
+            if (s.Age < 10)
+            {
+                throw new StudentAgeException("Student age is not valid");
+            }
+
+            Console.WriteLine(s.Name);
+        }
+    }
+}
---
+++ CustomException/StudentAgeException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace CustomException
+{
+    public class StudentAgeException : Exception
+    {
+        public StudentAgeException()
+        {
+        }
+
+        public StudentAgeException(string message) : base(message)
+        {
+        }
+    }
+}
